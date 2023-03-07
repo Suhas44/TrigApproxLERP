db5163faff4d32b8dc40d2e976290c50dd9afef0
Suhas44 <suhas8@outlook.com>
2023-03-06 22:26:06 -0600

    Slope Calculations

--- src/TrigApproxLerp.h
+++ src/TrigApproxLerp.h
@@ -6,8 +6,8 @@
 
 #ifndef LERP_H
 #define LERP_H
-double calcSin(float angle);
-void populateLookupTable(int incrementer, float threshold, float decrementer);
-double approxSin(float angle);
+double calcSin(double angle);
+void populateLookupTable(int incrementer, double threshold, double decrementer);
 void addValue(double ***old_array, double degree, double sin, int *rows);
+int isThresholdMet(double curr, double **arr, int rows, int incrementer, double threshold);
 #endif
--- src/sin.c
+++ src/sin.c
@@ -16,30 +16,37 @@ int main() {
     PLT();
 }
 
-double calcSin(float angle) {
+double calcSin(double angle) {
     return sin(angle * (M_PI / 180));
 }
 
-void populateLookupTable(int incrementer, float threshold, float decrementer) {
+void populateLookupTable(int incrementer, double threshold, double decrementer) {
     int rows = 0;
     double **arr = NULL;
 
     addValue(&arr, 0, 0, &rows);
     addValue(&arr, incrementer, calcSin(incrementer), &rows);
-
-    addValue(&arr, 6, calcSin(6), &rows);
-    addValue(&arr, 7, calcSin(7), &rows);
-    addValue(&arr, 8, calcSin(8), &rows);
-
-    for (int i = 0; i < rows; i++) {
-        printf("[");
-        for (int j = 0; j < 2; j++) {
-            printf("%f,", arr[i][j]);
-        }
-        printf("]\n");
+    double curr = 2 * incrementer;
+    while (isThresholdMet(curr, arr, rows, incrementer, threshold)) {
+        printf("The slope between sin(%f) and sin(%f) = (%f) is less than 1 percent different between the slope of sin(%f) and sin(%f) = (%f)\n", curr, arr[rows - 1][0], (calcSin(curr) - arr[rows - 1][1]) / incrementer, arr[rows - 1][0], arr[rows - 2][0], ((arr[rows - 1][1] - arr[rows - 2][1]) / incrementer));
+        addValue(&arr, curr, calcSin(curr), &rows);
+        curr += incrementer;
     }
 };
 
+int isThresholdMet(double curr, double **arr, int rows, int incrementer, double threshold) {
+    double slope1 = (calcSin(curr) - arr[rows - 1][1]) / incrementer;
+    double slope2 = (arr[rows - 1][1] - arr[rows - 2][1]) / incrementer;
+
+    printf("SLOPE between sin(%f) and sin(%f): %f\n", curr, arr[rows - 1][0], slope1);
+    printf("SLOPE between sin(%f) and sin(%f): %f\n", arr[rows - 1][0], arr[rows - 2][0], slope2);
+    double percentDiff = 1 - (slope1 / slope2);
+    printf("Percent Difference: %f\n", percentDiff);
+    if (percentDiff > threshold) {
+        printf("PERCENT DIFF OF SLOPE OF %f and %f is %f, or greater than %f!\n", curr, arr[rows - 1][0], percentDiff, threshold);
+    }
+    return percentDiff < threshold;
+}
 void addValue(double ***old_array, double degree, double sin, int *rows) {
     double new_row[2] = {degree, sin};
     (*rows)++;
@@ -48,4 +55,4 @@ void addValue(double ***old_array, double degree, double sin, int *rows) {
     memcpy((*old_array)[*rows - 1], new_row, 2 * sizeof(double));
 }
 
-double approxSin(float angle);
+double approxSin(double angle);
