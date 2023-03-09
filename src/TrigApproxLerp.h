2013e0b6b7df821d57855ae201d0b6e5ebb9ed6f
Suhas44 <suhas8@outlook.com>
2023-03-08 22:17:38 -0600

    Finished!

--- src/TrigApproxLerp.h
+++ src/TrigApproxLerp.h
@@ -6,9 +6,16 @@
 
 #ifndef LERP_H
 #define LERP_H
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+double approxSin(double angle);
+double approxSinCustom(double angle, double incrementer, double threshold, double decrementer);
 double calcSin(double angle);
-void populateLookupTable(double incrementer, double threshold, double decrementer);
+double **populateLookupTable(double incrementer, double threshold, double decrementer);
 void continualAddition(double **arr, int *rows, double *incrementer, double *curr, double decrementer, double threshold);
 int isThresholdMet(double curr, double **arr, int rows, int incrementer, double threshold);
 void addValue(double ***old_array, double degree, double sin, int *rows);
+double lookup(double angle, double **arr);
 #endif
--- src/sin.c
+++ src/sin.c
@@ -5,26 +5,32 @@
  */
 
 #include "TrigApproxLERP.h"
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #define PLT(...) populateLookupTable(5, 0.01, 0.001)
 
-int main() {
-    PLT();
+double approxSin(double angle) {
+    double **arr = PLT();
+    return lookup(angle, arr);
+}
+
+double approxSinCustom(double angle, double incrementer, double threshold, double decrementer) {
+    double **arr = populateLookupTable(incrementer, threshold, decrementer);
+    return lookup(angle, arr);
 }
 
 double calcSin(double angle) {
     return sin(angle * (M_PI / 180));
 }
 
-void populateLookupTable(double incrementer, double threshold, double decrementer) {
+double **populateLookupTable(double incrementer, double threshold, double decrementer) {
     int rows = 0;
     double **arr = NULL;
 
     addValue(&arr, 0, 0, &rows);
+    if (incrementer > 30) {
+        printf("WARNING: The incrementer will be too large to produce meaningful values between sin(0) and sin(%f). Please decrease the incrementer to less than 30!\n", incrementer);
+        exit(1);
+    }
     addValue(&arr, incrementer, calcSin(incrementer), &rows);
     double curr = 2 * incrementer;
     while (incrementer > 0.01 && curr < 90) {
@@ -38,6 +44,7 @@ void populateLookupTable(double incrementer, double threshold, double decremente
         }
     }
     addValue(&arr, 90, 1, &rows);
+    return arr;
 };
 
 int isThresholdMet(double curr, double **arr, int rows, int incrementer, double threshold) {
@@ -54,4 +61,27 @@ void addValue(double ***old_array, double degree, double sin, int *rows) {
     memcpy((*old_array)[*rows - 1], new_row, 2 * sizeof(double));
 }
 
-double approxSin(double angle);
+double lookup(double gAngle, double **arr) {
+    double angle = (int)gAngle % 360 + (gAngle - (int)gAngle);
+    int negate = 0;
+    if (angle < 0)
+        angle = 360 + angle;
+    if (angle > 90 && angle <= 180) {
+        angle = 180 - angle;
+    } else if (angle > 180 && angle <= 270) {
+        angle = angle - 180;
+        negate = 1;
+    } else if (angle > 270 && angle < 360) {
+        angle = 360 - angle;
+        negate = 1;
+    }
+    if (angle == 90)
+        return 1;
+    int len = 0;
+    while (arr[len] != NULL && arr[len][0] < angle) {
+        len++;
+    }
+    double slope = (arr[len][1] - arr[len - 1][1]) / (arr[len][0] - arr[len - 1][0]);
+    double answer = negate ? -1 * arr[len - 1][1] + (slope * (angle - arr[len][0])) : arr[len - 1][1] + (slope * (angle - arr[len][0]));
+    return answer;
+}
